e8bd2e3d958d5e9a49ffa3974caefba404a5c0b5
Cosmic <154437292+cosmicdesignio@users.noreply.github.com>
2024-01-15 03:33:49 +0000

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,1 +1,15 @@
-//we will have gsap animation here testing
+const nav = gsap.timeline({
+  defaults: {
+    duration: 0.5,
+    ease: "power2.inOut",
+  },
+});
+
+nav.from(".navbar1_logo-link", {
+  opacity: 0,
+});
+
+nav.from(".navbar1_link", {
+  opacity: 0,
+  stagger: 0.25,
+});
